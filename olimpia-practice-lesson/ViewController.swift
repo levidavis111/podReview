80aad50a4edace304eab1b85f7d7a4ef6bc6c300
levi davis <levidavis@pursuit.org>
2019-11-07 10:42:32 -0500

    First commit

---
+++ olimpia-practice-lesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  olimpia-practice-lesson
+//
+//  Created by Levi Davis on 11/7/19.
+//  Copyright © 2019 Levi Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
